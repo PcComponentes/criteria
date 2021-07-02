82511b63da33a16b2dc75e813dd9449e72fada2a
Jose Flores <jose.flores@pccomponentes>
2021-07-02 11:05:13 +0200

    add new filter operators

--- src/Domain/Criteria/FilterOperator.php
+++ src/Domain/Criteria/FilterOperator.php
@@ -8,7 +8,9 @@ final class FilterOperator extends EnumValueObject
 {
     const EQUAL    = '=';
     const GT       = '>';
+    const GTE       = '>=';
     const LT       = '<';
+    const LTE       = '<=';
     const CONTAINS = 'CONTAINS';
     const IN = 'IN';
     const NOT_IN = 'NOT IN';
