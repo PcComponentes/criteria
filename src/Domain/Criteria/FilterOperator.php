d54c61044c3347b53eff135cae27e79e8146949f
Jose Flores <jose.flores@pccomponentes>
2021-07-02 12:07:44 +0200

    change semantic const values

--- src/Domain/Criteria/FilterOperator.php
+++ src/Domain/Criteria/FilterOperator.php
@@ -8,9 +8,9 @@ final class FilterOperator extends EnumValueObject
 {
     const EQUAL    = '=';
     const GT       = '>';
-    const GTE       = '>=';
+    const GTE       = 'GREATER_THAN_OR_EQUAL';
     const LT       = '<';
-    const LTE       = '<=';
+    const LTE       = 'LESS_THAN_OR_EQUAL';
     const CONTAINS = 'CONTAINS';
     const IN = 'IN';
     const NOT_IN = 'NOT IN';
