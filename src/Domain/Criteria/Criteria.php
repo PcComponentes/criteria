adf46e0735fa149c2f00b5fa236a4f3f39861b25
cristian.almohalla <cristian.almohalla@pccomponentes.com>
2022-05-01 21:13:44 +0200

    feat: allow specifying multiple fields for sorting

--- src/Domain/Criteria/Criteria.php
+++ src/Domain/Criteria/Criteria.php
@@ -6,20 +6,35 @@ class Criteria
 {
     private $filters;
     private $order;
+    private $sorting;
     private $offset;
     private $limit;
 
-    public function __construct(Filters $filters, ?Order $order, ?int $offset, ?int $limit)
+    public function __construct(Filters $filters, $sorting, ?int $offset, ?int $limit)
     {
+        if ($sorting instanceof Order || null === $sorting) {
+            @trigger_error(
+                'Field type Order is deprecated, use Pccomponentes\Criteria\Domain\Criteria\Sorting instead',
+                E_USER_DEPRECATED,
+            );
+            $this->order = $sorting;
+            $this->sorting = Sorting::from([]);
+        } elseif ($sorting instanceof Sorting) {
+            $this->order = null;
+            $this->sorting = $sorting;
+        } else {
+            throw new \InvalidArgumentException('Invalid sorting field type at: ' . __FILE__);
+        }
+
         $this->filters = $filters;
-        $this->order = $order;
+
         $this->offset = $offset;
         $this->limit = $limit;
     }
 
-    public static function from(Filters $filters, ?Order $order, ?int $offset, ?int $limit): self
+    public static function from(Filters $filters, Sorting $sorting, ?int $offset, ?int $limit): self
     {
-        return new self($filters, $order, $offset, $limit);
+        return new self($filters, $sorting, $offset, $limit);
     }
 
     public function hasFilters(): bool
@@ -27,9 +42,21 @@ class Criteria
         return $this->filters->count() > 0;
     }
 
-    public function hasOrder()
+    /**
+     * @deprecated use sorting instead, variable order will be removed
+     */
+    public function hasOrder(): bool
     {
-        return null !== $this->order;
+        @trigger_error(
+            'Field type Order is deprecated, use Pccomponentes\Criteria\Domain\Criteria\Sorting instead',
+            E_USER_DEPRECATED,
+        );
+        return null !== $this->order || false === $this->sorting->isEmpty();
+    }
+
+    public function hasSorting(): bool
+    {
+        return null !== $this->order || false === $this->sorting->isEmpty();
     }
 
     public function filters(): Filters
@@ -37,9 +64,21 @@ class Criteria
         return $this->filters;
     }
 
+    /**
+     * @deprecated use sorting instead, variable order will be removed
+     */
     public function order(): ?Order
     {
+        @trigger_error(
+            'Field type Order is deprecated, use Pccomponentes\Criteria\Domain\Criteria\Sorting instead',
+            E_USER_DEPRECATED,
+        );
         return $this->order;
+    }
+
+    public function sorting(): Sorting
+    {
+        return $this->sorting;
     }
 
     public function offset(): ?int
