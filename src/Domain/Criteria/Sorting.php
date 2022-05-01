95ecad301950077efd48af80d901476508851b6a
cristian.almohalla <cristian.almohalla@pccomponentes.com>
2022-05-01 20:58:43 +0200

    feat: add sorting object

---
+++ src/Domain/Criteria/Sorting.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Pccomponentes\Criteria\Domain\Criteria;
+
+use Assert\Assert;
+use PcComponentes\Ddd\Domain\Model\ValueObject\CollectionValueObject;
+
+final class Sorting extends CollectionValueObject
+{
+    /**
+     * @param Order[] $items
+     */
+    public static function from(array $items): self
+    {
+        Assert::that($items)->all()->isInstanceOf(Order::class);
+
+        return parent::from($items);
+    }
+
+    public static function create(Order...$items): self
+    {
+        return parent::from($items);
+    }
+}
