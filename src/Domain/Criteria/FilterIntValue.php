2bedaa11fc370734c966f8551184c96e52995843
cristian.almohalla <cristian.almohalla@pccomponentes.com>
2022-12-28 11:52:07 +0100

    feat: add filter int value

---
+++ src/Domain/Criteria/FilterIntValue.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Pccomponentes\Criteria\Domain\Criteria;
+
+use PcComponentes\Ddd\Domain\Model\ValueObject\IntValueObject;
+
+final class FilterIntValue extends IntValueObject implements FilterValueInterface
+{
+}
